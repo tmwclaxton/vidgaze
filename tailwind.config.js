68783ec046cd7f89238c5f24501d5fa32bc19113
tmwcl <tmwclaxton@gmail.com>
2023-02-25 21:00:52 +0000

    prepare tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: 'class',
+    mode: 'jit',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -10,12 +13,65 @@ module.exports = {
     ],
 
     theme: {
+        screens: {
+            xs: "470px",
+            // => @media (min-width: 640px) { ... }
+            sm: "640px",
+            // => @media (min-width: 640px) { ... }
+
+            md: "768px",
+            // => @media (min-width: 768px) { ... }
+
+            ld: "868px",
+            // => @media (min-width: 768px) { ... }
+
+            lg: "1024px",
+            // => @media (min-width: 1024px) { ... }
+
+            ml: "1124px",
+            // => @media (min-width: 1124px) { ... }
+
+            xl: "1280px",
+            // => @media (min-width: 1280px) { ... }
+
+            "2xl": "1536px",
+            // => @media (min-width: 1536px) { ... }
+
+            "4xl": "1920px",
+            // => @media (min-width: 1920px) { ... }
+        },
+
+
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            aspectRatio: {
+                "21/12": "21 / 12",
+                "9/16": "9 / 16",
+                "20/27": "20 / 27",
+            },
+            height: {
+                "10v": "10vh",
+                "20v": "20vh",
+                "30v": "30vh",
+                "40v": "40vh",
+                "50v": "50vh",
+                "60v": "60vh",
+                "70v": "70vh",
+                "80v": "80vh",
+                "90v": "90vh",
+                "100v": "100vh",
+            },
+            colors: {
+                "vidgaze-blue": "#0e0e0e",
+            }
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/line-clamp"),
+    ],
 };
