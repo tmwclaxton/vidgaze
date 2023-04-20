dd2e135eb7d60764646d9fa27c81fda2d10220fc
tmwcl <tmwclaxton@gmail.com>
2023-04-20 21:51:41 +0100

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,10 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        {{--favicon--}}
+        <link rel="icon" href="{{ asset('images/logos/vidgaze/logo.png') }}" type="image/x-icon">
+        <link rel="shortcut icon" href="{{ asset('images/logos/vidgaze/logo.png') }}" type="image/x-icon">
+
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
