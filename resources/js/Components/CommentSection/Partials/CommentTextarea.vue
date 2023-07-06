ede5e58bcb9fe2c2e72c1877330e09a10a8f0605
tmwcl <tmwclaxton@gmail.com>
2023-07-06 21:59:35 +0100

    [watchpage] add honeypot trap, add auto resize for CommentTextarea.vue

--- resources/js/Components/CommentSection/Partials/CommentTextarea.vue
+++ resources/js/Components/CommentSection/Partials/CommentTextarea.vue
@@ -4,6 +4,7 @@ import QuaternaryButton from "@/Components/Buttons/QuaternaryButton.vue";
 
 const name = 'CommentTextarea';
 const commentOptions = ref(false);
+const comment = ref('');
 const props = defineProps({
     item: {
         type: Object,
@@ -15,25 +16,45 @@ const props = defineProps({
         default: null
     }
 });
+
+const submit = () => {
+    try {
+        this.$refs.honeypot.validate()
+    } catch (error) {
+        // error handling
+    }
+    comment.value = '';
+}
+
+const resizeTextarea = (event) => {
+    event.target.style.height = '5px';
+    event.target.style.height = event.target.scrollHeight + 'px';
+}
+
 </script>
 
 
 <template>
-    <textarea @click="commentOptions = true" class="h-9 mb-3 mt-3 w-full peer block p-2 resize-none text-sm text dark:textDark  bg-transparent
+    <form @submit.prevent="submit" class="w-full">
+        <textarea   v-model="comment" @input="resizeTextarea($event)"
+            @click="commentOptions = true" class="h-9 mb-3 mt-3 w-full peer block p-2 resize-none text-sm text dark:textDark  bg-transparent
           border-t-0 border-x-0 border-b-1 border-zinc-300 focus:border-zinc-400 dark:border-zinc-600 dark:focus:border-zinc-400
           focus:border-1 focus:ring-0 overflow-y-hidden"
-              placeholder="Leave a comment..."></textarea>
+                 placeholder="Leave a comment..."></textarea>
 
-    <div v-show="commentOptions" class=" justify-end w-full flex  ">
+        <VueHoneyPot ref="honeypot"/>
 
-        <QuaternaryButton class="mr-2" @click="commentOptions = false;">
-            <font-awesome-icon :icon="['fas', 'ban']" class="w-4 h-4"/>
-            <span class="font-semibold">Cancel</span>
-        </QuaternaryButton>
-        <QuaternaryButton class="mr-2" @click="commentOptions = false;">
-            <font-awesome-icon :icon="['fas', 'comment']" class="w-4 h-4"/>
-            <span class="font-semibold">Comment</span>
-        </QuaternaryButton>
+        <div v-show="commentOptions" class=" justify-end w-full flex  ">
 
-    </div>
+            <QuaternaryButton class="mr-2" @click="commentOptions = false;">
+                <font-awesome-icon :icon="['fas', 'ban']" class="w-4 h-4"/>
+                <span class="font-semibold">Cancel</span>
+            </QuaternaryButton>
+            <QuaternaryButton class="mr-2" @click="commentOptions = false; submit">
+                <font-awesome-icon :icon="['fas', 'comment']" class="w-4 h-4"/>
+                <span class="font-semibold">Comment</span>
+            </QuaternaryButton>
+
+        </div>
+    </form>
 </template>
