2200130767879e90041cf26d6b1ed29a17c7fd1b
tmwcl <tmwclaxton@gmail.com>
2023-08-26 00:57:28 +0100

    playlist video component

---
+++ resources/js/Pages/Viewer/Feed/Playlist/Partials/PlaylistVideo.vue
@@ -0,0 +1,42 @@
+<script setup>
+const name = "PlaylistVideo";
+
+const props = defineProps({
+    video: {
+        type: Object,
+        required: true
+    },
+    playlist: {
+        type: Object,
+        required: false
+    }
+});
+
+</script>
+<template>
+    <div class="relative flex   group z-0">
+        <div>
+            <div class="flex-shrink-0  relative rounded overflow-hidden  @if($playlist) h-24 @else h-16 @endif aspect-video bg-vidgaze-blue  ">
+
+                <img class="object-cover w-full h-full bg-zinc-900" :src="video.thumbnail_url" alt=""/>
+
+                <div class="absolute rounded overflow-hidden w-min bottom-0 right-0 m-1">
+                    <div class="relative w-full text-white font-semibold bg-black
+                      px-auto flex flex-row   rounded-sm text-sm dark:text-zinc-200 justify-center">
+                        <p class="text-center p-0.5 text-xs" v-text="video.duration"></p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="w-full ">
+            <div class="w-full flex align-bottom flex-col leading-none pl-2 ">
+                <p style="" class="text dark:textDark text-md font-bold leading-4 line-clamp-2 pr-2 mt-1" v-text="video.title"/>
+                <div  class="text-xs text dark:textDark w-max my-1" v-text="video.creator.name"/>
+            </div>
+        </div>
+
+    </div>
+
+</template>
--- resources/js/Pages/Viewer/Feed/Playlist/Playlist.vue
+++ resources/js/Pages/Viewer/Feed/Playlist/Playlist.vue
@@ -3,8 +3,11 @@ import {onMounted, ref, watch} from "vue";
 import {useAuthStore} from "@/Stores/AuthStore";
 import {usePlaylistModalStore} from "@/Stores/PlaylistModalStore";
 import PlaylistLock from "@/Components/Cards/PlaylistCard/Partials/PlaylistLock.vue";
+import RowDivider from "@/Components/General/RowDivider.vue";
+import SubscribeButton from "@/Components/Buttons/SubscribeButton.vue";
+import PlaylistVideo from "@/Pages/Viewer/Feed/Playlist/Partials/PlaylistVideo.vue";
 
-const playlist = ref([]);
+const playlist = ref(null);
 const videos = ref([]);
 
 onMounted(async () => {
@@ -25,31 +28,29 @@ onMounted(async () => {
 </script>
 <template>
     <Head title="" />
-    <div class=" mx-auto ">
-
 
 
             <div class="flex flex-col xs:flex-row max-h-[calc(100vh-4rem)] overflow-hidden">
 
                 <!--playlist details-->
-                <div class="xs:bg-zinc-200 dark:xs:bg-zinc-900 w-full xs:w-72 md:w-96 flex-shrink-0 pt-5 px-5">
+                <div v-if="playlist != null" class="w-full xs:w-72 md:w-96 flex-shrink-0 pt-5 px-5">
                     <div class="relative rounded-lg overflow-hidden aspect-video bg-vidgaze-blue w-full cursor-pointer ">
                         <img v-if="playlist.recent_video_image !== null" class="object-cover w-full h-full " v-bind:src="playlist.recent_video_image" />
-                        <div class="absolute rounded overflow-hidden w-full bottom-0 right-0  ">
+                        <div class="absolute   overflow-hidden w-full bottom-0 right-0  ">
                             <a href="/watch"
-                               class="relative w-full text-white font-semibold bg-black opacity-80 px-auto flex flex-row   rounded-sm text-sm dark:text-zinc-200 justify-center">
+                               class="relative w-full text-white font-semibold bg-black opacity-80 px-auto flex flex-row  text-sm dark:text-zinc-200 justify-center">
                                 <p class="text-center py-2 uppercase">Play all</p>
                             </a>
                         </div>
                     </div>
                     <div class="flex flex-row p-1 -mb-2">
-                        <p class="text dark:textDark text-xl font-semibold" v-text="playlist.name"></p>
+                        <p class=" text-xl font-semibold" v-text="playlist.name"></p>
 
 
                     </div>
 
 
-                    <div class=" space-y-1 text dark:textDark px-1">
+                    <div class=" space-y-1  px-1">
                         <p class="inline-flex mr-2" v-text="playlist.video_count + ' · ' + 'Updated ' + playlist.updated_at + ' · ' "></p>
                         <PlaylistLock :visibility="playlist.visibility" class="inline-flex w-3 mx-1"/>
                         <div class="inline-flex relative ml-1.5">
@@ -60,34 +61,42 @@ onMounted(async () => {
                     </div>
 
 
-                    <div class="relative mt-3 mx-1 w-full flex flex-row space-x-4 text dark:textDark">
-                        <a href="/watch/" class="m-0 p-0 flex flex-col">
-                            <!--<x-icon name="shuffle" class=" w-4 mt-1" />-->
-                        </a>
+                    <div class="relative mt-3 mx-1 w-full flex flex-row space-x-4 ">
+                        <div class="m-0 p-0 flex flex-col">
+                            <font-awesome-icon :icon="['fas', 'shuffle']" class="w-4 mt-1"></font-awesome-icon>
+                        </div>
+                        <div class="m-0 p-0 flex flex-col">
+                            <font-awesome-icon :icon="['fas', 'share']" class="w-4 mt-1"></font-awesome-icon>
+                        </div>
                     </div>
 
 
                     <p class="text text-sm px-1 " v-text="playlist.description"/>
-                    <x-hr class="mt-5 mb-5"/>
+                    <row-divider class="mt-5 mb-5 rounded-2xl"></row-divider>
+
                     <div class="flex flex-row px-2 mb-5">
                         <a href="/channel/" class="flex flex-row">
                             <img class="bg-white h-10 aspect-square rounded-full "
-                                 src="">
-                            <div class=" flex flex-col my-auto px-5 overflow-hidden">
-                                <span class="text dark:textDark text-lg font-bold  break-words"></span>
-                            </div>
+                                 :src="playlist.creator.avatar_url"/>
                         </a>
-                        <div class="my-auto  ml-auto flex flex-row gap-x-2">
-                        <!--    subscribe button-->
+                        <div class="flex flex-col ml-2">
+                            <a href="/channel/" class="text text-sm font-semibold dark:text-zinc-200"
+                               v-text="playlist.creator.name"></a>
+                            <p class="text text-sm dark:text-zinc-200" v-text="playlist.creator.subscriber_count"></p>
+                        </div>
+                        <div v-if="useAuthStore().user === null" class="my-auto  ml-auto flex flex-row gap-x-2">
+                            <SubscribeButton :channel="playlist.creator" class="my-auto"/>
                         </div>
                     </div>
                 </div>
 
                 <!--playlist videos-->
-                <div class=" w-full min-h-screen h-full">
+                <div class=" w-full min-h-screen h-full xs:bg-zinc-200 dark:xs:bg-zinc-900 ">
+                    <div class="h-full overflow-y-auto m-5 flex flex-col gap-y-5">
+                        <PlaylistVideo v-if="videos.length > 0"
+                            v-for="video in videos" :key="video.id" :video="video" class="w-full"/>
+                    </div>
 
                 </div>
             </div>
-
-        </div>
 </template>
