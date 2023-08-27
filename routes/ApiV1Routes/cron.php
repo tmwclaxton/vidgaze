6a98e0037ddb53d5a479db6d961a5bd5561b9538
tmwcl <tmwclaxton@gmail.com>
2023-08-28 00:51:35 +0100

    add tested comments beside cron endpoints

--- routes/ApiV1Routes/cron.php
+++ routes/ApiV1Routes/cron.php
@@ -5,10 +5,10 @@ use App\Http\Controllers\ApiControllers\CronApiController;
 Route::middleware(['vidgaze.api.key'])->group(function () {
     Route::prefix('/cron')->name('cron.')->group(function () {
 
-        // telescope prune endpoint
+        // telescope prune endpoint // tested
         Route::post('/telescope/prune', [CronApiController::class, 'telescope'])->name('telescope.prune');
 
-        // prune sanctum tokens
+        // prune sanctum tokens // tested
         Route::post('/sanctum/prune', [CronApiController::class, 'sanctumTokens'])->name('sanctum.tokens.prune');
 
         // refresh one twitch category
@@ -20,16 +20,16 @@ Route::middleware(['vidgaze.api.key'])->group(function () {
         // refresh streams
         Route::post('/refresh/streams', [CronApiController::class, 'refreshStreams'])->name('refresh.streams');
 
-        // prune old live viewers
+        // prune old live viewers // tested
         Route::post('/live_viewers/prune', [CronApiController::class, 'liveViewersPrune'])->name('live_viewers.prune');
 
         // refresh subscriptions
         Route::post('/refresh/subscriptions', [CronApiController::class, 'refreshSubscriptions'])->name('refresh.subscriptions');
 
-        // store logs to s3
+        // store logs to s3 // tested
         Route::post('/logs/backup', [CronApiController::class, 'backupLogs'])->name('logs.backup');
 
-        // prune batch logs
+        // prune batch logs // tested
         Route::post('/batches/prune', [CronApiController::class, 'pruneBatches'])->name('batches.prune');
 
     });
