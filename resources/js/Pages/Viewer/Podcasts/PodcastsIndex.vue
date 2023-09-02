76d95e42ad1437a8cca4aa88ab5e0b1577678733
tmwcl <tmwclaxton@gmail.com>
2023-09-02 07:06:06 +0100

    under construction page for studio stream

--- resources/js/Pages/Viewer/Podcasts/PodcastsIndex.vue
+++ resources/js/Pages/Viewer/Podcasts/PodcastsIndex.vue
@@ -27,10 +27,10 @@ export default {
         <div class=" mx-auto   h-[calc(100vh-4rem)] py-auto justify-center flex flex-col">
             <div class="mx-10 my-auto flex-col  flex   dark:bg-transparent  ">
 
-                <div class="flex flex-col space-y-2 items-center justify-center animate-pulse">
+                <div class="flex flex-col space-y-2 items-center justify-center anim ate-pulse">
                     <div class="w-52 h-52 rounded-full p-8 bg-zinc-300 dark:bg-zinc-200 animate-bou nce">
                         <!--<font-awesome-icon :icon="['fas', 'wrench']" class="w-full h-full text-zinc-600 dark:text-zinc-400 animate-bounce"/>-->
-                        <font-awesome-icon :icon="['fas', 'otter']"  class="w-full h-full text-zinc-600 dark:text-vidgaze-blue"/>
+                        <font-awesome-icon :icon="['fas', 'wrench']"  class="w-full h-full text-zinc-600 dark:text-vidgaze-blue"/>
                     </div>
                     <p class="  text-zinc-600 dark:textDark font-bold text-4xl pt-8 text-center">VidGaze Podcasts is Under Construction</p>
                     <!--<p class="text dark:textDark text-sm justify-center text-justify ">-->
