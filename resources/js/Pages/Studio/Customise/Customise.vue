c54942258e4dc37b59337e59da846e394efcbfb0
tmwcl <tmwclaxton@gmail.com>
2023-09-07 23:10:40 +0100

    fix customise page bug

--- resources/js/Pages/Studio/Customise/Customise.vue
+++ resources/js/Pages/Studio/Customise/Customise.vue
@@ -36,13 +36,13 @@ const updateChannelDetails = () => {
 
 <template>
     <div  v-if="useAuthStore().user != null">
-            <div class=" flex flex-col sm:flex-row-reverse ">
-                <div class="display:initial   mx-8 sm:mr-8 sm:ml-0">
-                    <div class="sticky top-36 pt-3 w-full sm:w-56">
+            <div class=" flex flex-col md:flex-row-reverse ">
+                <div class="display:initial   mx-8 md:mr-8 md:ml-0">
+                    <div class="sticky top-36 pt-3 w-full md:w-56">
                         <consistent-content-holder class="">
-                            <div class="pb-3  overflow-hidden rounded w-full  mb-2 sm:ml-0 ">
-                                <div class="mx-12 my-4  flex justify-center">
-                                    <img class="w-1/2 sm:w-full aspect-square rounded-full "
+                            <div class="pb-3  overflow-hidden rounded w-full  mb-2 md:ml-0 ">
+                                <div class="md:mx-12 my-4  flex justify-center">
+                                    <img class="w-1/2 md:w-full aspect-square rounded-full "
                                             :src="useAuthStore().user.creator.avatar_url"
                                             alt="Profile picture">
                                 </div>
@@ -65,78 +65,80 @@ const updateChannelDetails = () => {
                 </div>
 
 
-                <consistent-padding class=" flex-grow h-full">
-                    <TitleComponent :text="'Customise your channel'" class="  mb-8">
-                        <font-awesome-icon icon="user-cog" class="w-6 h-6 my-auto"/>
-                    </TitleComponent>
-                    <div class="flex flex-col gap-y-3">
-                        <!--channel name-->
-                        <!--<TextInput v-model="name" label="Channel name" class="mb-4" placeholder="Channel name"/>-->
-                        <StudioTextInput :value="useAuthStore().user.creator.name"
-                                         @update:model-value="useAuthStore().user.creator.name = $event"
-                                         @submit="updateChannelDetails()"
-                                         label="Channel Name"
-                                         placeholder="Channel Name"
-                                         for="channel-name"
-                        />
+                <div class="flex flex-grow">
+                    <consistent-padding class=" flex-grow h-full">
+                        <TitleComponent :text="'Customise your channel'" class="  mb-8">
+                            <font-awesome-icon icon="user-cog" class="w-6 h-6 my-auto"/>
+                        </TitleComponent>
+                        <div class="flex flex-col gap-y-3">
+                            <!--channel name-->
+                            <!--<TextInput v-model="name" label="Channel name" class="mb-4" placeholder="Channel name"/>-->
+                            <StudioTextInput :value="useAuthStore().user.creator.name"
+                                             @update:model-value="useAuthStore().user.creator.name = $event"
+                                             @submit="updateChannelDetails()"
+                                             label="Channel Name"
+                                             placeholder="Channel Name"
+                                             for="channel-name"
+                            />
 
-                        <!--channel bio-->
-                        <StudioTextInput :value="useAuthStore().user.creator.bio"
-                                        @update:model-value="useAuthStore().user.creator.bio = $event"
-                                        @submit="updateChannelDetails()"
-                                        label="Channel bio"
-                                        placeholder="Channel bio"
-                                        for="channel-bio"
-                                        :enter-submit="false"
-                                         maxlength="1000"
-                        />
+                            <!--channel bio-->
+                            <StudioTextInput :value="useAuthStore().user.creator.bio"
+                                             @update:model-value="useAuthStore().user.creator.bio = $event"
+                                             @submit="updateChannelDetails()"
+                                             label="Channel bio"
+                                             placeholder="Channel bio"
+                                             for="channel-bio"
+                                             :enter-submit="false"
+                                             maxlength="1000"
+                            />
 
-                        <!--channel profile image-->
-                        <StudioImageInput :value="useAuthStore().user.creator.avatar_url"
-                                          :endpoint="route('api.creator.update.avatar')"
-                                          label="Channel Profile Image"
-                                          description="Your profile picture will appear where your channel is presented on VidGaze, like next to your videos and comments."
-                                          recommendation="We recommend using a square image (1:1 aspect ratio) with a minimum resolution of 98x98 pixels and a maximum file size of 4MB."
-                                          placeholder="Channel Profile Image"
-                                          for="channel-profile-image"
-                                          :rounded="true"
-                                          :enter-submit="false"
-                                          :max-file-size="4"
-                                          :max-width="98"
-                                          :max-height="98"
-                                          :aspect-ratio="1"
-                                          :file-type="['image/png','image/jpeg']"
-                        />
+                            <!--channel profile image-->
+                            <StudioImageInput :value="useAuthStore().user.creator.avatar_url"
+                                              :endpoint="route('api.creator.update.avatar')"
+                                              label="Channel Profile Image"
+                                              description="Your profile picture will appear where your channel is presented on VidGaze, like next to your videos and comments."
+                                              recommendation="We recommend using a square image (1:1 aspect ratio) with a minimum resolution of 98x98 pixels and a maximum file size of 4MB."
+                                              placeholder="Channel Profile Image"
+                                              for="channel-profile-image"
+                                              :rounded="true"
+                                              :enter-submit="false"
+                                              :max-file-size="4"
+                                              :max-width="98"
+                                              :max-height="98"
+                                              :aspect-ratio="1"
+                                              :file-type="['image/png','image/jpeg']"
+                            />
 
-                        <!--channel banner image-->
-                        <StudioImageInput :value="useAuthStore().user.creator.banner_url"
-                                            :endpoint="route('api.creator.update.banner')"
-                                          label="Channel Profile Banner"
-                                          description="Your banner will appear at the top of your channel page."
-                                          recommendation="We recommend using a 16:9 aspect ratio image with a minimum resolution of 2048x1152 pixels and a maximum file size of 6MB."
-                                          placeholder="Channel Profile Banner"
-                                          for="channel-profile-banner"
-                                          :rounded="false"
-                                          :max-file-size="6"
-                                          :max-width="4096"
-                                          :max-height="4096"
-                                          :aspect-ratio="16/9"
-                                          :file-type="['image/png','image/jpeg']"
-                        />
-                        <!--channel contact email-->
-                        <StudioTextInput :value="useAuthStore().user.creator.contact_email"
-                                         @update:model-value="useAuthStore().user.creator.contact_email = $event"
-                                         @submit="updateChannelDetails()"
-                                         label="Contact Email"
-                                         placeholder="Contact Email"
-                                         for="contact-email"
-                                         maxlength="320"
-                        />
+                            <!--channel banner image-->
+                            <StudioImageInput :value="useAuthStore().user.creator.banner_url"
+                                              :endpoint="route('api.creator.update.banner')"
+                                              label="Channel Profile Banner"
+                                              description="Your banner will appear at the top of your channel page."
+                                              recommendation="We recommend using a 16:9 aspect ratio image with a minimum resolution of 2048x1152 pixels and a maximum file size of 6MB."
+                                              placeholder="Channel Profile Banner"
+                                              for="channel-profile-banner"
+                                              :rounded="false"
+                                              :max-file-size="6"
+                                              :max-width="4096"
+                                              :max-height="4096"
+                                              :aspect-ratio="16/9"
+                                              :file-type="['image/png','image/jpeg']"
+                            />
+                            <!--channel contact email-->
+                            <StudioTextInput :value="useAuthStore().user.creator.contact_email"
+                                             @update:model-value="useAuthStore().user.creator.contact_email = $event"
+                                             @submit="updateChannelDetails()"
+                                             label="Contact Email"
+                                             placeholder="Contact Email"
+                                             for="contact-email"
+                                             maxlength="320"
+                            />
 
-                    </div>
+                        </div>
 
 
-                </consistent-padding>
+                    </consistent-padding>
+                </div>
             </div>
         </div>
 </template>
