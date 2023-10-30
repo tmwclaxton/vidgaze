a12ba90f81351ce9db5cd71e9411fcba1a6a9ecf
tmwcl <tmwclaxton@gmail.com>
2023-10-30 20:07:39 +0000

    modularising the content page into components, also unadulterated array in video resource

--- resources/js/Pages/Studio/Content/Content.vue
+++ resources/js/Pages/Studio/Content/Content.vue
@@ -5,18 +5,57 @@ import {Head} from "@inertiajs/vue3";
 import ConsistentPadding from "@/Layouts/Partials/ConsistentPadding.vue";
 import TitleComponent from "@/Components/General/TitleComponent.vue";
 import LikeDislikeBar from "@/Pages/Studio/Content/Partials/LikeDislikeBar.vue";
+import TableItem from "@/Pages/Studio/Content/Partials/TableItem.vue";
 
+const name = 'StudioContent';
+const selected = ref([]);
+const items = ref([]);
 
 onBeforeMount(async () => {
     await axios.get(route("api.studio.content")).then((response) => {
-        videos.value = response.data.videos.data;
-        video_drafts.value = response.data.videoDrafts.data
+        items.value = response.data.videos.data.concat(response.data.videoDrafts.data);
     });
 });
 
-const video_drafts = ref([]);
-const videos = ref([]);
+const toggleSelectAll = () => {
+    // if not all selected, select all
+    if (selected.value.length !== items.value.length) {
+        selected.value = [];
+        items.value.forEach((item) => {
+            selected.value.push(item.id);
+        });
+    } else {
+        selected.value = [];
+    }
+};
 
+const toggleSelect = (id) => {
+    if (selected.value.includes(id)) {
+        selected.value = selected.value.filter((item) => item !== id);
+    } else {
+        selected.value.push(id);
+    }
+};
+
+const findLink = (item) => {
+    if (item.type === 'video') {
+        return route('studio.video.edit', item.slug);
+    }
+    if (item.type === 'video draft') {
+        return route('studio.video.draft.edit', item.slug);
+    }
+    return '';
+};
+
+const thumbnail = (item) => {
+    if (item.type === "video") {
+        return item.thumbnail_url;
+    }
+    if (item.type === "video draft") {
+        return item.thumbnail_path;
+    }
+    return '';
+};
 </script>
 
 <template>
@@ -35,104 +74,51 @@ const videos = ref([]);
                 <thead
                     class="text-xs text-zinc-700 uppercase border-y-2 dark:border-zinc-800 dark:text-zinc-200">
                 <tr>
-                    <th scope="col" class="w-[300px]">
-<!--                        <div class="flex items-center" wire:click="toggleSelectAll()">-->
-<!--                            <input type="checkbox" {{ selectAll ? 'checked' : ''  }}-->
-<!--                            class="w-4 h-4 text-blue-600  border-zinc-300 rounded focus:ring-offset-0 focus:outline-none    focus:ring-0  ring-transparent dark:bg-zinc-700 dark:border-zinc-600">-->
-<!--                        </div>-->
+                    <th scope="col" class="p-4">
+                        <div class="flex items-center" @click="toggleSelectAll">
+                            <input type="checkbox" v-bind:checked="selected.length === items.length"
+                               class="rounded w-4 h-4 text-blue-600 bg-zinc-100 border-zinc-300 without-ring dark:bg-zinc-700 dark:border-zinc-600 hover:dark:bg-zinc-700 focus:dark:bg-zinc-700">
+                        </div>
                     </th>
-
+                    <th scope="col" class="w-[300px]">
+                    </th>
+                    <th scope="col" class="py-3 ">
+                        Type
+                    </th>
                     <th scope="col" class="py-3 ">
                         Visibility
                     </th>
                     <th scope="col" class="py-3 cursor-pointer">
-<!--                    <span class="flex flex-row">-->
-                        Date
-<!--                    </span>-->
+                    <span class="flex flex-row w-full justify-center">
+                        <p>Date</p>
+                    </span>
                     </th>
                     <th scope="col" class="py-3 ">
                         Views
+                    </th>
+                    <th scope="col" class="py-3 ">
+                        Live Viewers
                     </th>
                     <th scope="col" class="py-3  ">
                         Comments
                     </th>
                     <th scope="col" class="py-3">
-                        Likes vs Dislikes
+                        Likes (vs. dislikes)
                     </th>
+                    <!--<th scope="col" class="px-6 py-3 cursor-pointer" @click="toggleDirection">-->
+                    <!--    <span class="flex flex-row">-->
+                    <!--        Live viewers-->
+                    <!--        &lt;!&ndash;<x-icon name="extend-up"&ndash;&gt;-->
+                    <!--        &lt;!&ndash;        class="{{ sortAsc ? 'rotate-180' : ''  }}  fill w-2 ml-2"/>&ndash;&gt;-->
+                    <!--    </span>-->
+                    <!--</th>-->
                 </tr>
                 </thead>
-                <tbody class="h-full">
-                    <tr v-for="videoDraft in video_drafts"
-                        class="border-b-2 hover:bg-zinc-200 dark:hover:bg-zinc-900 dark:border-zinc-800">
-                        <td>
-                            <div class="ml-2 my-2 flex space-x-3 max-h-[90px] max-w-[300px]">
-                                <Link :href="route('studio.video.draft.edit', videoDraft.slug)">
-                                    <img src="https://picsum.photos/1600/900" class="w-[160px] h-[90px] rounded-lg">
-                                </Link>
-                                <div class="max-w-[120px]">
-                                    <Link :href="route('studio.video.draft.edit', videoDraft.slug)">
-                                        <h3 class="font-semibold text-lg hover:underline">{{ videoDraft.title }}</h3>
-                                    </Link>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            {{ videoDraft.visibility }}
-                        </td>
-                        <td>
-                            {{ videoDraft.created_at }}
-                        </td>
-                        <td>
-                            N/A
-                        </td>
-                        <td>
-                            N/A
-                        </td>
-                        <td>
-                            N/A
-                        </td>
-                    </tr>
-                    <tr v-for="video in videos"
-                        class="border-b-2 hover:bg-zinc-200 dark:hover:bg-zinc-900 dark:border-zinc-800">
-                        <td>
-                            <div class="ml-2 my-2 flex space-x-3 max-h-[90px] max-w-[300px]">
-                                <Link :href="route('studio.video.edit', video.slug)">
-                                    <img :src="video.thumbnail_url" class="w-[160px] h-[90px] rounded-lg">
-                                </Link>
-                                <div class="max-w-[120px]">
-                                    <Link :href="route('studio.video.edit', video.slug)">
-                                        <h3 class="font-semibold text-lg hover:underline">{{ video.title }}</h3>
-                                    </Link>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            {{ video.visibility }}
-                        </td>
-                        <td>
-                            <div v-if="video.time_published">
-                                <p>{{ video.time_published }}</p>
-                                <p class="text-xs text-zinc-500 dark:text-zinc-400">Published</p>
-                            </div>
-                            <div v-else="video.time_uploaded">
-                                <p>{{ video.time_uploaded }}</p>
-                                <p class="text-xs text-zinc-500 dark:text-zinc-400">Uploaded</p>
-                            </div>
-                        </td>
-                        <td>
-                            {{ video.view_count }}
-                        </td>
-                        <td>
-                            {{ video.comment_count }}
-                        </td>
-                        <td>
-<!--                            {{ video.like_count }} ({{ video.dislike_count }})-->
-<!--                            make a bar of green and red for like to dislike ratio-->
-                            <LikeDislikeBar :dislikes="video.dislike_count" :likes="video.like_count"/>
-
-
-                        </td>
-                    </tr>
+                <tbody class="h-full" :key="selected">
+                    <TableItem v-for="item in items" :key="item.id" :item="item" :link="findLink(item)" :thumbnail="thumbnail(item)"
+                               @toggleSelect="toggleSelect($event)"
+                               :selected="selected.includes(item.id)"
+                    />
                 </tbody>
             </table>
 <!--        </div>-->
