9345b22b3526d8a1550f748c78f66374086f593b
Joshua Young <joshuasy10mc@gmail.com>
2023-11-08 21:55:11 +0000

    VV-39: stream population

--- app/Console/Commands/RefreshStreams.php
+++ app/Console/Commands/RefreshStreams.php
@@ -32,7 +32,7 @@ class RefreshStreams extends Command
     {
         Twitch::updateStreamerStatus();
 
-        Twitch::updateTopCategories(3, 3);
+        Twitch::updateTopCategories(8, 3);
 
         return Command::SUCCESS;
     }
