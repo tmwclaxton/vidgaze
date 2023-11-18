9adc1c2a19a85f33f8daa5c61478d219383e4f44
tmwcl <tmwclaxton@gmail.com>
2023-11-18 15:55:27 +0000

    [PWA-TESTING] remove vuex, add vite-pwa, add app icon

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,29 @@ import Vue from '@vitejs/plugin-vue'
 import Markdown from 'vite-plugin-vue-markdown'
 import * as path from "path";
 import svgLoader from 'vite-svg-loader';
+import {VitePWA} from "vite-plugin-pwa";
 export default defineConfig({
     plugins: [
-
+        VitePWA({
+            injectManifest: true,
+            injectRegister: true,
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true
+            },
+            manifest: {
+                name: 'VidGaze',
+                short_name: 'VidGaze',
+                theme_color: '#080a0f',
+                icons: [
+                    {
+                        src: '/images/logos/vidgaze/app_icon_512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
         laravel({
             input: 'resources/js/app.js',
             refresh: true,
