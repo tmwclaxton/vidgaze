fba9fb747ab658ef507da5ef852dea88531a3566
Toby Claxton <57643714+tmwclaxton@users.noreply.github.com>
2023-11-19 20:28:22 +0000

    [VV-107] fix subscription page visual defect, fix search defect, fix NotificationCard.vue defect, remove whitespace (#27)

--- resources/js/Pages/Viewer/Feed/Subscriptions/SubscriptionsIndex.vue
+++ resources/js/Pages/Viewer/Feed/Subscriptions/SubscriptionsIndex.vue
@@ -24,35 +24,28 @@ onMounted(() => {
 });
 
 </script>
-
-
-
 <template>
     <Head>
         <title>Subscriptions</title>
     </Head>
 
-
-    <ConsistentPadding class="px-12">
+    <ConsistentPadding>
         <div class="flex flex-row w-full  justify-center">
-            <div class="flex flex-row  flex-grow my-auto">
-                <SubscriptionsIcon class="text dark:textDark w-5 aspect-square mr-2 "/>
-                <p class="text dark:textDark font-bold text-md h-full my-auto ">Subscriptions</p>
-            </div>
+            <TitleComponent text="Subscriptions">
+                <font-awesome-icon :icon="['fas', 'heart']" class="my-auto h-5"/>
+            </TitleComponent>
             <Link :href="route('feed.channels') " class=" text-center my-auto flex flex-row gap-x-2 ml-auto">
                 <font-awesome-icon :icon="['fas', 'gear']" class="my-auto h-5"/>
                 <p class="text dark:textDark font-bold text-md h-full ">Manage</p>
-
             </Link>
         </div>
 
-        <row-divider class="mt-6 mb-3 rounded-2xl"></row-divider>
-
+        <row-divider class="mt-4 mb-3 rounded-2xl opacity-0"></row-divider>
 
         <div class="flex flex-col gap-y-2" v-if="streams.length > 0">
-            <TitleComponent text="Livestreams" class="my-4 mb-8">
-                <font-awesome-icon :icon="['fas', 'fire']" class="w-6 h-6 my-auto" />
-            </TitleComponent>
+<!--            <TitleComponent text="Livestreams" class="my-4 mb-8">-->
+<!--                <font-awesome-icon :icon="['fas', 'tower-broadcast']" class="h-6 w-6 my-auto" /> -->
+<!--            </TitleComponent>-->
 
             <div class="px-3 xs:px-0 flex
                         grid grid-cols-1 xs:grid-cols-2 ld:grid-cols-3 lg:grid-cols-4 lg:grid-cols-5 4xl:grid-cols-6 gap-4 " >
@@ -60,12 +53,14 @@ onMounted(() => {
             </div>
         </div>
 
-        <div class="flex flex-col gap-y-2" v-if="videos.length > 0">
-            <TitleComponent text="Latest" class="my-4 mb-8">
-                <font-awesome-icon :icon="['fas', 'fire']" class="w-6 h-6 my-auto" />
-            </TitleComponent>
+        <row-divider class="mt-6 mb-3 rounded-2xl" v-if="videos.length > 0 && streams.length > 0"></row-divider>
 
-            <div class="px-3 xs:px-0 flex
+        <div class="flex flex-col gap-y-2" v-if="videos.length > 0">
+<!--            <TitleComponent text="Latest" class="my-4 mb-8">-->
+<!--                <font-awesome-icon :icon="['fas', 'fire']" class="w-6 h-6 my-auto" />-->
+<!--            </TitleComponent>-->
+
+            <div class="pt-6 px-3 xs:px-0 flex
                         grid grid-cols-1 xs:grid-cols-2 ld:grid-cols-3 lg:grid-cols-4 lg:grid-cols-5 4xl:grid-cols-6 gap-4 ">
                 <VideoStreamCard v-for="video in videos" :key="video.id" :item="video" />
             </div>
@@ -74,10 +69,5 @@ onMounted(() => {
         <div class="mt-20" v-if="loaded && videos.length === 0 && streams.length === 0 ">
             <ErrorMessage :message="'Whoops you have no new content'"/>
         </div>
-
     </ConsistentPadding>
-
-
-
-
 </template>
