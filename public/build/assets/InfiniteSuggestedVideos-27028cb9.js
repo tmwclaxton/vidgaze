ae2c750e6330d81a396433cb40294bbd30cc8a7e
Toby Claxton <57643714+tmwclaxton@users.noreply.github.com>
2023-11-25 22:22:45 +0000

    VV-114 PWA (#29)
    * [PWA-TESTING] remove vuex, add vite-pwa, add app icon
    
    * [pwa-testing] changes
    
    Signed-off-by: tmwcl <tmwclaxton@gmail.com>
    
    ---------
    
    Signed-off-by: tmwcl <tmwclaxton@gmail.com>

--- public/build/assets/InfiniteSuggestedVideos-27028cb9.js
+++
@@ -1,1 +0,0 @@
-const n={__name:"InfiniteSuggestedVideos",props:{video:{type:Object,required:!0}},setup(e){return(t,r)=>null}};export{n as default};
--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,35 @@ import Vue from '@vitejs/plugin-vue'
 import Markdown from 'vite-plugin-vue-markdown'
 import * as path from "path";
 import svgLoader from 'vite-svg-loader';
+import {VitePWA} from "vite-plugin-pwa";
 export default defineConfig({
     plugins: [
+        VitePWA({
+            // injectManifest: false,
+            // injectRegister: false,
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true
+            },
 
+            manifest: {
+                name: 'VidGaze',
+                short_name: 'VidGaze',
+                description: 'VidGaze is a video sharing platform',
+                theme_color: '#080a0f',
+                start_url: '/',
+                scope: '/',
+                display: 'standalone',
+                lang: 'en',
+                icons: [
+                    {
+                        src: '/images/logos/vidgaze/app_icon_512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
         laravel({
             input: 'resources/js/app.js',
             refresh: true,
