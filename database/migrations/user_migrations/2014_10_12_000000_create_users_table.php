d9329affe710deb9cb2efffb611e765936b91bef
tmwcl <tmwclaxton@gmail.com>
2025-02-27 21:00:40 +0000

    add user role enum

--- database/migrations/user_migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/user_migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->date('DOB')->nullable();
             $table->string('password');
-
+            $table->enum('role', ['user', 'moderator','admin'])->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
